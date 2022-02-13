6b1763fa4a091b7f0c06c4b6826141504ecb5773
Liioni <stohr@kth.se>
2022-02-13 16:51:29 +0100

    Most of the enemy select functions are now in place.
    Both popups works, and you get different information depending on what opponent is picked. Need to connect the battle-button to the game scene.

---
+++ source/scenes/opponent_choice/popup_data.gd
@@ -0,0 +1,16 @@
+extends Popup
+
+onready var picture = get_node("picture")
+onready var description = get_node("description")
+onready var name_popup = get_node("name_popup")
+
+func _ready():
+	pass
+
+func _on_back_button_pressed():
+	hide()
+
+
+func _on_battle_button_pressed():
+	#get_tree().change_scene(TODO, ADD GAME SCENE)
+	pass
