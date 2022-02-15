46505bccafbf3103c42b48e092720712d3fc1116
Daverinoe <daverinoe@gmail.com>
2022-02-15 20:46:41 +0100

    Attack juice
    Attack juiced up, screen shake and everything

--- source/autoloads/event.gd
+++ source/autoloads/event.gd
@@ -1,3 +1,4 @@
 extends Node
 
-signal emit_audio(audio) # send an Dictionary: {"bus": "bus_choice", "type": "audio_choice"}
+signal emit_audio(audio) # send an Dictionary: {"bus": "bus_choice", "choice": "audio_choice"}
+signal add_shake(shake_amount)
