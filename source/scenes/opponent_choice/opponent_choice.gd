8e4bf37981a48f9a7c12d0447aa342b258c43a70
Liioni <stohr@kth.se>
2022-02-16 21:08:51 +0100

    added some new random logic

--- source/scenes/opponent_choice/opponent_choice.gd
+++ source/scenes/opponent_choice/opponent_choice.gd
@@ -26,16 +26,25 @@ func _ready():
 	
 
 func fill_opponents_array():
+	rng.randomize()
 	for god in gods:
-		opponent_array.append(opponent_button.new(gods[god]["name"], gods[god]["description"]))
+		if god == "zeus":
+			pass
+		else:
+			opponent_array.append(opponent_button.new(gods[god]["name"], gods[god]["description"]))
+	opponent_array.shuffle()
+	opponent_array.append(opponent_button.new(gods["zeus"]["name"], gods["zeus"]["description"]))
+
 
 func add_opponents():
-	rng.randomize()
-	var number_one = rng.randi_range(0,opponent_array.size() -1)
-	var number_two = rng.randi_range(0,opponent_array.size() -1)
-	while number_one == number_two:
-		number_two = rng.randi_range(0,opponent_array.size() -1)
-	
-	opponent_one_spot.add_child(opponent_array[number_one])
-	opponent_two_spot.add_child(opponent_array[number_two])
+#	rng.randomize()
+#	var number_one = rng.randi_range(0,opponent_array.size() -1)
+#	var number_two = rng.randi_range(0,opponent_array.size() -1)
+#	while number_one == number_two:
+#		number_two = rng.randi_range(0,opponent_array.size() -1)
+	if opponent_array.size() < 2:
+		opponent_array.empty()
+		fill_opponents_array()
+	opponent_one_spot.add_child(opponent_array.pop_front())
+	opponent_two_spot.add_child(opponent_array.pop_front())
 
