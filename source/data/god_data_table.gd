302f0d47a775ca0062a95684a9878526cae8fdf4
Liioni <stohr@kth.se>
2022-02-17 13:11:01 +0100

    updated name of a god and added new gods to the list

--- source/data/god_data_table.gd
+++ source/data/god_data_table.gd
@@ -4,11 +4,13 @@ extends Resource
 const GodStats = preload("res://source/data/god_data.gd")
 
 var data = {
-	"zeus": GodStats.new("zeus", "Zeus Lorem ipsum dolor sit amet,"),
-	"amon_ra": GodStats.new("amon_ra", "amon_ra Lorem ipsum dolor sit amet,"),
-	"chicomecoat": GodStats.new("chicomecoat", "chicomecoat Lorem ipsum dolor sit amet,"),
-	"gaia": GodStats.new("gaia", "gaia Lorem ipsum dolor sit amet,"),
-	"hades": GodStats.new("hades", "hades Lorem ipsum dolor sit amet,")
+	"zeus": GodStats.new("zeus", "The King of the gods"),
+	"amon_ra": GodStats.new("amon_ra", "Sun god - One of the creator gods - head of a hawk"),
+	"chicomecoatl": GodStats.new("chicomecoatl", "Goddess of agriculture"),
+	"gaia": GodStats.new("gaia", "The Earth Goddess"),
+	#"poseidon": GodStats.new("poseidon", "The God of the Seas"),
+	#"dionysus": GodStats.new("dionysus", "The God of Wine"),
+	"hades": GodStats.new("hades", "The God of the Underworld")
 	}
 
 func get_data():
