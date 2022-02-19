615cdfbfe22ad1dbac20a28c96f576e64eb61f84
Orbitoid <Orbitoid@users.noreply.github.com>
2022-02-19 19:17:13 +1100

    updated background for game screen, updated some god text (still more to go)

--- source/data/god_data_table.gd
+++ source/data/god_data_table.gd
@@ -6,24 +6,24 @@ var data = {
 		"zeus",
 		"The King of the gods",
 		{
-			"open_taunt": ["Hahaha, you dare challenge me!?"],
-			"round_draw": ["Hmmm...", "Ugh..."],
-			"round_lose": ["No!", "Ouch!", "Grrr..."],
-			"round_win": ["I've got you now!", "Another step closer to victory!", "Calculated."],
-			"game_lose": ["How is that possible?"],
-			"game_win": ["Hahaha, I didn't doubt it for a second!"],
+			"open_taunt": ["You are mere moments away from heaven now!", "By Hades or by might, your fate is sealed!"],
+			"round_draw": ["It is of no matter...", "Ugh..."],
+			"round_lose": ["No!", "Hm!", "Oh, for heaven's sake!"],
+			"round_win": ["Heaven help you!", "Prayers alone cannot help you now!", "Finally a round worthy of my name!", "...and the Lord taketh away!"],
+			"game_lose": ["Defeated by a mortal!? IMPOSSIBLE!", "Blasphemy!!"],
+			"game_win": ["It is as I ordained.", "It was written in the heavens"],
 		}
 	),
 	"amon_ra": GodData.new(
 		"amon_ra",
-		"Sun god - One of the creator gods - head of a hawk",
+		"Egyptian Sun God",
 		{
-			"open_taunt": ["Hahaha, you dare challenge me!?"],
+			"open_taunt": ["Such a stupid mortal. You will never best my strategy!", "You cannot outsmart me!"],
 			"round_draw": ["Hmmm...", "Ugh..."],
-			"round_lose": ["No!", "Ouch!", "Grrr..."],
-			"round_win": ["I've got you now!", "Another step closer to victory!", "Calculated."],
-			"game_lose": ["How is that possible?"],
-			"game_win": ["Hahaha, I didn't doubt it for a second!"],
+			"round_lose": ["Humph", "I'll just have to change things up a bit!"],
+			"round_win": ["How can you ever expect to win!?", "Another step closer to victory!", "Calculated."],
+			"game_lose": ["But I executed the plan!??", "Next time I'll outsmart you!"],
+			"game_win": ["I knew I had the winning strategy!"],
 		},
 		[9, 0, 0]
 	),
@@ -31,12 +31,12 @@ var data = {
 		"chicomecoatl",
 		"Goddess of agriculture",
 		{
-			"open_taunt": ["Hahaha, you dare challenge me!?"],
+			"open_taunt": ["Let's see what you got!"],
 			"round_draw": ["Hmmm...", "Ugh..."],
-			"round_lose": ["No!", "Ouch!", "Grrr..."],
-			"round_win": ["I've got you now!", "Another step closer to victory!", "Calculated."],
-			"game_lose": ["How is that possible?"],
-			"game_win": ["Hahaha, I didn't doubt it for a second!"],
+			"round_lose": ["My plan wilters!", "Ouch!", "Grrr..."],
+			"round_win": ["Things are really starting to bloom!", "You look like you could use some sunshine. Have you spoken to Ra recently?", "It is harvesting season!"],
+			"game_lose": ["I'm all shriveled up!"],
+			"game_win": ["What a bountiful harvest!", "It was only a matter of sun, water and time!"],
 		}
 	),
 	"gaia": GodData.new(
@@ -45,10 +45,10 @@ var data = {
 		{
 			"open_taunt": ["Hahaha, you dare challenge me!?"],
 			"round_draw": ["Hmmm...", "Ugh..."],
-			"round_lose": ["No!", "Ouch!", "Grrr..."],
-			"round_win": ["I've got you now!", "Another step closer to victory!", "Calculated."],
+			"round_lose": ["Is it getting hotter in here?", "Ouch!", "Grrr..."],
+			"round_win": ["", "Another step closer to victory!", "Calculated."],
 			"game_lose": ["How is that possible?"],
-			"game_win": ["Hahaha, I didn't doubt it for a second!"],
+			"game_win": ["Mother nature always finds a way."],
 		}
 	),
 	"poseidon": GodData.new(
