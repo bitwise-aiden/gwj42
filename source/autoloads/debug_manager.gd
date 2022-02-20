ebf30c5211d82df42e7f526fa31ae9e5ee16d751
velopman <74576289+velopman@users.noreply.github.com>
2022-02-20 11:07:17 -0500

    Add screenshots and tutorial

--- source/autoloads/debug_manager.gd
+++ source/autoloads/debug_manager.gd
@@ -1,37 +1,54 @@
 extends Node
 
+# Private constants
 
-var __screenshot_count = 0
+const __OUTPUT_DIRECTORY: String = "screenshots"
+const __OUTPUT_FILE_NAME: String = "%04d-%02d-%02d_%02d.%02d.%02d.png"
 
-var __is_debug_mode = false
+
+# Private variables
+
+var __activate_prev: bool = false
+var __activate_curr: bool = true
+
 
 # Lifecycle methods
+
 func _ready() -> void:
-	if !OS.is_debug_build():
-		self.call_deferred("queue_free")
+	IOHelper.directory_create(__OUTPUT_DIRECTORY)
 
 	self.pause_mode = Node.PAUSE_MODE_PROCESS
 
 
-func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_A) && Input.is_key_pressed(KEY_B):
+func _process(_delta: float) -> void:
+	# TODO: Update to input action
+
+	__activate_prev = __activate_curr
+	__activate_curr = Input.is_key_pressed(KEY_S)
+
+	var pressed = !__activate_prev && __activate_curr
+
+	if Input.is_key_pressed(KEY_CONTROL) && Input.is_key_pressed(KEY_SHIFT) && pressed:
 		self.__screenshot()
 
-# Put this into _input to prevent key from being registered multiple times because delta is super fast
-func _input(event: InputEvent) -> void:
-	if Input.is_key_pressed(KEY_F1):
-		if !__is_debug_mode:
-			self.__show_debug_info()
-		__is_debug_mode = !__is_debug_mode
 
 # Private methods
+
 func __screenshot():
 	var capture: Image = self.get_viewport().get_texture().get_data()
-	capture.flip_y()
-	capture.save_png("user://screenshot_%d.png" % self.__screenshot_count)
-	self.__screenshot_count += 1
-	OS.shell_open(OS.get_user_data_dir())
 
-func __show_debug_info():
-	Logger.info("Debug info showing.")
-	# TODO: Add cursor position, FPS, CPU, RAM, GPU utilisation
+	capture.flip_y()
+
+	var datetime: Dictionary = OS.get_datetime(true)
+	var file_name: String = __OUTPUT_FILE_NAME % [
+		datetime["year"],
+		datetime["month"],
+		datetime["day"],
+		datetime["hour"],
+		datetime["minute"],
+		datetime["second"],
+	]
+
+	var capture_result: int = capture.save_png("user://%s/%s" % [__OUTPUT_DIRECTORY, file_name])
+	if capture_result != OK:
+		pass # TODO: Call logger
